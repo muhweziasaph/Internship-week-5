a0ab12486842d72aa9e5f11c05deafd85dd235dc
MUHWEZI ASAPH <muhweziasaph33@gmail.com>
2025-08-19 23:41:52 +0300

    Update Book.js

--- day3-4-5-backend-app/models/Book.js
+++ day3-4-5-backend-app/models/Book.js
@@ -4,7 +4,7 @@ const bookSchema = new mongoose.Schema(
   {
     _id : {
      type: Number,
-      required: [true, '_id is required'], 
+      required: [true, 'Published year is required'], 
    },
     title: {
       type: String,
@@ -29,6 +29,7 @@ const bookSchema = new mongoose.Schema(
       enum: ['Fiction', 'Non-fiction', 'Biography', 'Science', 'Fantasy', 'Other'],
       default: 'Other',
     },
+  },
+  { timestamps: true }
 );
-
 module.exports = mongoose.model('Book', bookSchema);
